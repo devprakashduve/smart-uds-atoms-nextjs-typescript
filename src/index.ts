488126718e912e1e406001d82322fea164a136dc
devprakashduve <dev876.1@gmail.com>
2025-02-21 22:26:45 +0530

    fix(UDS-17): module inprogress23

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Avatar from './Components/Atoms/Avatar';
 import Button from './Components/Atoms/Button';
 import Icon from './Components/Atoms/Icon';
-import Img from './Components/Atoms/Image';
+import Image from './Components/Atoms/Image';
 import Label from './Components/Atoms/Label';
 import Checkbox from './Components/Atoms/InputGroup/Checkbox';
 import Input from './Components/Atoms/InputGroup/Input';
@@ -12,12 +12,18 @@ import Slider from './Components/Atoms/InputGroup/Slider';
 import Switch from './Components/Atoms/InputGroup/Switch';
 import TextArea from './Components/Atoms/InputGroup/TextArea';
 import List from './Components/Atoms/List';
+import Badge from './Components/Atoms/Badge';
+import CustomLink from './Components/Atoms/CustomLink';
+import Divider from './Components/Atoms/Divider';
 
 export {
   Avatar,
+  Badge,
   Button,
+  CustomLink,
+  Divider,
   Icon,
-  Img,
+  Image,
   Label,
   Checkbox,
   Input,
