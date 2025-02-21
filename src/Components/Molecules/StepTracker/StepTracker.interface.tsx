9773135794c220fc925868a06d6d85c657cab5e8
devprakashduve <dev876.1@gmail.com>
2025-02-21 20:21:54 +0530

    fix(UDS-17): module inprogress10

--- src/Components/Molecules/StepTracker/StepTracker.interface.tsx
+++
@@ -1,9 +0,0 @@
-export interface StepTrackerProps {
-  currentStepIndex: number; // The current step index in the progress
-  totalStepsCount: number; // Total number of steps
-  containerClassName?: string; // Optional additional className for the container
-  nextButtonText?: string; // Optional text for the next button
-  onNextStep?: () => void; // Optional callback for next step button click
-  orientation?: 'horizontal' | 'vertical'; // Optional orientation property
-  size?: 'sm' | 'md' | 'lg'; // Optional size property
-}
