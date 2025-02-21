936350c9c91838d0d9fb815cebf7d5b65afca9d4
devprakashduve <dev876.1@gmail.com>
2025-02-21 20:54:31 +0530

    fix(UDS-17): module inprogress12

--- src/Components/Molecules/Table/index.tsx
+++ src/Components/Molecules/Table/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { TableProps } from './TableProps.interface';
-import Button from '@/Components/Atoms/Button';
-// import './Table.css';
+import Pagination from '@/Components/Molecules/Pagination';
 
 const Table = <T,>({
   data,
@@ -18,12 +17,12 @@ const Table = <T,>({
 
   return (
     <div className={`table-container ${className || ''}`}>
-      <table className="min-w-full border-collapse border border-gray-300">
-        <thead className="bg-gray-100 text-left">
+      <table className="min-w-full border-collapse border border-atom-btn/30">
+        <thead className="bg-atom-btn/70 text-left">
           <tr>
             {columns.map((col) => (
               <th
-                className={`border-b border-gray-300 px-4 py-2 text-sm font-semibold ${col.className || ''}`}
+                className={`border-b border-atom-btn/30 px-4 py-4 text-sm font-semibold ${col.className || ''}`}
                 key={String(col.key)}
               >
                 {col.header}
@@ -35,13 +34,13 @@ const Table = <T,>({
           {paginatedData.map((row, rowIndex) => (
             <tr
               key={rowIndex}
-              className="cursor-pointer border-b border-gray-300 transition-all hover:bg-blue-50"
+              className="cursor-pointer border-b border-atom-btn/30 transition-all hover:bg-atom-btn/30"
               onClick={() => onRowClick?.(row)}
             >
               {columns.map((col) => (
                 <td
                   key={String(col.key)}
-                  className={`border-r border-gray-300 px-4 py-2 ${col.className || ''}`}
+                  className={`border-r border-atom-btn/30 px-4 py-2 ${col.className || ''}`}
                 >
                   {col.render
                     ? col.render(row[col.key], row)
@@ -52,31 +51,12 @@ const Table = <T,>({
           ))}
         </tbody>
       </table>
-      <div className="pagination-controls mt-4 flex justify-center">
-        <Button
-          className="mx-1 rounded border border-gray-300 px-4 py-2"
-          onClick={() => onPageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          Previous
-        </Button>
-        {Array.from({ length: totalPages }, (_, index) => (
-          <Button
-            key={index}
-            className={`mx-1 rounded border border-gray-300 px-4 py-2 ${currentPage === index + 1 ? 'bg-blue-500 text-white' : ''}`}
-            onClick={() => onPageChange(index + 1)}
-          >
-            {index + 1}
-          </Button>
-        ))}
-        <Button
-          className="mx-1 rounded border border-gray-300 px-4 py-2"
-          onClick={() => onPageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-        >
-          Next
-        </Button>
-      </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={onPageChange}
+        className="mt-4 flex justify-center"
+      />
     </div>
   );
 };
