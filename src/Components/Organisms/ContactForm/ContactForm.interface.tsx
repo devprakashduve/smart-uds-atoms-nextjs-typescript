dccdbd6b5a13904add916fb140f199c7d9296a15
devprakashduve <dev876.1@gmail.com>
2025-03-01 11:40:00 +0530

    fix(UDS-25): general form added fix 7

--- src/Components/Organisms/ContactForm/ContactForm.interface.tsx
+++ src/Components/Organisms/ContactForm/ContactForm.interface.tsx
@@ -1,5 +1,4 @@
 import { ContactInformationProps } from '../ContactInformation/types';
-import { TextAreaProps } from '../../../Components/Atoms/InputGroup/TextArea/TextAreaProps.interface';
 import { FormHandlerProps } from '../FormHandler/FormHandler.interface';
 
 export interface ContactFormProps {
